136f2670beb6b4dbf18a7c3f9289693e811afbd3
Ignacio Buioli <ibuioli@gmail.com>
2024-01-10 16:57:13 -0300

    feat: re-org

--- __manifest__.py
+++ __manifest__.py
@@ -1,33 +1,25 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "mass_payments_group",
+    'name': "Mass Payments Group",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'summary': """Mass Payments Group""",
 
     'description': """
-        Long description of module's purpose
+        Mass Payments Group
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Exemax",
+    'website': "https://www.exemax.com.ar",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
     'depends': ['base', 'account', 'account_payment_group'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-	'models/models.py'
+        'wizard/views.xml',
     ],
-    # only loaded in demonstration mode
     'demo': [],
 }
